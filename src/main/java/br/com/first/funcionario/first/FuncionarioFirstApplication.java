553a8b27875f96403035f9e7d4d0619c08b6a77f
Ariane Pinheiro <arianepinheirodev@gmail.com>
2024-11-25 22:26:52 -0300

    inicia projeto

---
+++ src/main/java/br/com/first/funcionario/first/FuncionarioFirstApplication.java
@@ -0,0 +1,13 @@
+package br.com.first.funcionario.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionarioFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/first/funcionario/first/FuncionarioFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.first.funcionario.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
