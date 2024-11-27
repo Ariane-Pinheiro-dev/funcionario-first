64c43fb8a54a2b2b7f849ec899cb93a292fafbd3
Ariane Pinheiro <arianepinheirodev@gmail.com>
2024-11-26 21:09:39 -0300

    teste infra ok

--- src/main/java/br/com/first/funcionario/first/FuncionarioFirstApplication.java
+++ src/main/java/br/com/first/funcionario/first/FuncionarioFirstApplication.java
@@ -2,12 +2,23 @@ package br.com.first.funcionario.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class FuncionarioFirstApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FuncionarioFirstApplication.class, args);
-	}
+    @GetMapping
+    public String getHomeTeste() {
+        return "Funcionario First - API Home";
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuncionarioFirstApplication.class, args);
+    }
 
 }
